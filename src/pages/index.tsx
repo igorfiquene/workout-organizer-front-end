1784f40bc9544975166685b6e030b279d5fb4a3a
igorfiquene <igorfpessoa@gmail.com>
2023-04-24 20:27:18 -0300

    feat: initial page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,25 @@
+import { Button, Flex, Text, Title } from '@mantine/core'
+import { IconBarbell } from '@tabler/icons-react'
+import Link from 'next/link'
 
 export default function Home() {
-  // return <Title order={1}>Painel</Title>
+  return (
+    <Flex direction={'column'} gap={'md'} sx={{ maxWidth: '30rem' }}>
+      <Title order={1}>Top, vamos planejar seu próximo treino?</Title>
+
+      <Text size="lg">Veja só como é fácil criar uma rotina de treino</Text>
+
+      <Button
+        size="md"
+        radius="xl"
+        leftIcon={<IconBarbell />}
+        variant="light"
+        color="violet"
+      >
+        <Link style={{ textDecoration: 'none' }} href={'/treinos'}>
+          Crie um treino
+        </Link>
+      </Button>
+    </Flex>
+  )
 }
